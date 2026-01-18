3ae4ebb8ee0d1a960d20cb2be790daef64120954
jahidIqbalBiswas <official.jahidbiswas@gmail.com>
2026-01-19 00:22:46 +0600

    implement data provider

---
+++ src/constants/index.ts
@@ -0,0 +1,13 @@
+export const DEPARTMENTS =  [
+    "QURAN",
+    "HADITH",
+    "CSE",
+    "MATH",
+    "ENGLISH",
+    "LLB",
+    "ROBOTICS"
+]
+export const DEPARTMENT_OPTIONS = DEPARTMENTS.map(dept => ({
+    value: dept,
+    label: dept
+}))
